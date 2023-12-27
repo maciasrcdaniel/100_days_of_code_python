d383acf023018ef36ab5fc3fe9f8098adc6c3f36
maciasrcdaniel <maciasrcdaniel@gmail.com>
2023-12-27 00:54:23 -0800

    Initial commit

---
+++ treasure_island_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# import module
+import time
+
+# -- Treasure Island Game Function
+def treasure_island_game():
+    
+    # -- Welcome message 
+    print("Welcome to Treasure Island.")
+
+    time.sleep(1)
+
+    print("""
+            88           88                                 88  
+            ""           88                                 88  
+                         88                                 88  
+            88 ,adPPYba, 88 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  
+            88 I8[    "" 88 ""     `Y8 88P'   `"8a a8"    `Y88  
+            88  `"Y8ba,  88 ,adPPPPP88 88       88 8b       88  
+            88 aa    ]8I 88 88,    ,88 88       88 "8a,   ,d88  
+            88 `"YbbdP"' 88 `"8bbdP"Y8 88       88  `"8bbdP"Y8  
+            """)
+
+    print("Your mission is to find the treasure.")
+
+  
+
