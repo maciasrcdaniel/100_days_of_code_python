3fb0ec5b113e7d1a21151b8c946f1849e1f73daf
maciasrcdaniel <danielmacias@Daniels-MacBook-Pro.local>
2024-01-17 23:10:16 -0800

    Update Prime Checker App

--- exercises/prime_number_checker.py
+++ exercises/prime_number_checker.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 
 
-n = int(input("Pick a number"))
+n = int(input("Pick a number\n"))
 
 def prime_checker(number):
-    counter_range = range(1, 17)
-    # list of primes
-    list_of_primes = [2,3]
-
-    # Find primes
-    for i in counter_range:
-        minus_prime = ((6 * i) - 1)
-        plus_prime = ((6 * i) + 1)
-        list_of_primes.append(minus_prime)
-        list_of_primes.append(plus_prime)
-
-    if number in list_of_primes:
-        print("Prime Number")
+    number_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            number_prime = False
+    if number_prime:
+        print("Prime")
     else:
         print("Not Prime")
+
 
 prime_checker(number=n)
